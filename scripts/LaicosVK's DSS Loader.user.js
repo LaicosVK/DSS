e3cc7b5509f90d89b7a8ef7344d32916eda3fec4
LaicosVK <vincent.kresse@gmail.com>
2022-10-19 17:16:13 +0200

    Update LaicosVK's DSS Loader.user.js

--- scripts/LaicosVK's DSS Loader.user.js
+++ scripts/LaicosVK's DSS Loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LaicosVK's DSS Loader
 // @namespace    https://die-staemme.de/
-// @version      2
+// @version      3
 // @description  Läd den LVK Script loader
 // @author       LaicosVK
 // @match        https://*.die-staemme.de/game.php*

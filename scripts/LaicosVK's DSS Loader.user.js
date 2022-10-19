f51bb686eb01a6cb5865a4bd80d79184535d8bdd
LaicosVK <vincent.kresse@gmail.com>
2022-10-19 16:52:06 +0200

    Update LaicosVK's DSS Loader.user.js

--- scripts/LaicosVK's DSS Loader.user.js
+++ scripts/LaicosVK's DSS Loader.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         LaicosVK's DSS Loader
 // @namespace    https://die-staemme.de/
-// @version      1.0
+// @version      1.1
 // @description  Läd den LVK Script loader
 // @author       LaicosVK
 // @match        https://*.die-staemme.de/game.php*
 // @grant        none
 
-// @updateURL    https://github.com/LaicosVK/DSS/raw/main/scripts/loader%20loader.user.js
-// @downloadURL  https://github.com/LaicosVK/DSS/raw/main/scripts/loader%20loader.user.js
+// @updateURL    https://github.com/LaicosVK/DSS/raw/main/scripts/LaicosVK's%20DSS%20Loader.user.js
+// @downloadURL  https://github.com/LaicosVK/DSS/raw/main/scripts/LaicosVK's%20DSS%20Loader.user.js
 
 // @require      https://github.com/LaicosVK/DSS/raw/main/scripts/Raubzugrechner.user.js
 // @require      https://github.com/LaicosVK/DSS/raw/main/scripts/Quick%20select.user.js
